15f435d80fdae0542df02e0664f170e52854faaa
eshtokin <eshtoki@gmail.com>
2022-06-06 00:57:45 +0300

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Alex Eshtokin on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
